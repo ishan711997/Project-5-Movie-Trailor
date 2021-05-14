fc15adf69958edbdd5429976236eae76acdab76f
ishan711997 <ishan711997@gmail.com>
2021-05-14 15:46:01 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    // $('p').append(`<i class="fa fa-star" id="s1"></i>
+    // <i class="fa fa-star" id="s2"></i>
+    // <i class="fa fa-star" id="s3"></i>
+    // <i class="fa fa-star" id="s4"></i>
+    // <i class="fa fa-star" id="s5"></i>`).children();
+        
+    //     $("#s1").click(function(){
+    //         $(".fa-star").css({"color":"#212529"});
+    //         $(this).css({"color":"yellow"});
+    //     });
+    //     $("#s2").click(function(){
+    //         $(".fa-star").css({"color":"#212529"});
+    //         $("#s2").prevAll().css({"color":"yellow"});
+    //         $(this).css({"color":"yellow"});
+    //     });
+    //     $("#s3").click(function(){
+    //         $(".fa-star").css({"color":"#212529"});
+    //         $("#s3").prevAll().css({"color":"yellow"});
+    //         $(this).css({"color":"yellow"});
+    //     });
+    //     $("#s4").click(function(){
+    //         $(".fa-star").css({"color":"#212529"});
+    //         $("#s4").prevAll().css({"color":"yellow"});
+    //         $(this).css({"color":"yellow"});
+    //     });
+    //     $("#s5").click(function(){
+    //         $(".fa-star").css({"color":"#212529"});
+    //         $("#s5").prevAll().css({"color":"yellow"});
+    //         $(this).css({"color":"yellow"});
+    //     });
+    
+});
